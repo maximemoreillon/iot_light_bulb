9b8dca121d4a03fa9e39bf558b9f0b5f846faf7f
Maxime Moreillon <m.moreillon@gmail.com>
2022-01-10 11:17:18 +0900

    iot-kernel

--- misc.ino
+++
@@ -1,8 +0,0 @@
-String get_device_name(){
-  String chip_id = String(ESP.getChipId(), HEX);
-  return String(DEVICE_TYPE) + "-" + chip_id;
-}
-
-String get_device_nickname(){
-  return read_string_from_eeprom(EEPROM_DEVICE_NICKNAME_ADDRESS);
-}
